b625369143efaa5b79c1290a75dcb26eaf08121f
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-08-20 16:50:34 -0300

    Add files via upload

--- ListaMatriz/Ex1.java
+++ ListaMatriz/Ex1.java
@@ -1,16 +1,20 @@
 package ListaMatriz;
 
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Ex1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner entrada = new Scanner(System.in);
 		
 		int TAM = 0, maior = 0, menor = 0, soma = 0, count = 0, countPares = 0, countImpares = 0, somaDiagoPrin = 0, somaDiagoSec= 0;
 		float media = 0;
 		
 		System.out.println("::.. Operações com Matriz ..::");
+		
 		do{
 		System.out.print("Informe um número inteiro ímpar entre 3 e 11(inclusive 3 e 11): ");
 		TAM = entrada.nextInt();
@@ -55,23 +59,47 @@ public class Ex1 {
 				}
 			}
 			
+			FileWriter arquivo = new FileWriter("C:/Users/c.agronomia/Documents/Matriz.txt");
+			PrintWriter gravarArquivo = new PrintWriter(arquivo);
+			
+			gravarArquivo.print("Dados Matriz");
 			System.out.println("\nA soma dos valores informados é: "+soma);
+			gravarArquivo.println("\nA soma dos valores informados é: "+soma);
+			
 			media = soma /count;
 			System.out.println("A média é igual a: "+media);
+			gravarArquivo.println("A média é igual a: "+media);
+			
 			System.out.println("Maior valor informado: "+maior);
+			gravarArquivo.println("Maior valor informado: "+maior);
+			
 			System.out.println("Menor valor informado: "+menor);
+			gravarArquivo.println("Menor valor informado: "+menor);
+			
 			System.out.println("Quantidade de pares informados: "+countPares);
+			gravarArquivo.println("Quantidade de pares informados: "+countPares);
+			
 			System.out.println("Quantidade de ímpares informados: "+countImpares);
+			gravarArquivo.println("Quantidade de ímpares informados: "+countImpares);
+			
 			System.out.println("A soma da diagonal principal é igual a: "+somaDiagoPrin);
+			gravarArquivo.println("A soma da diagonal principal é igual a: "+somaDiagoPrin);
+			
 			System.out.println("A soma da diagonal secundária é igual a: "+somaDiagoSec);
+			gravarArquivo.println("A soma da diagonal secundária é igual a: "+somaDiagoSec);
+			
 			System.out.println("Matriz informada:");
+			gravarArquivo.println("Matriz informada:");
 			for(int i = 0; i < mat.length; i++){
 				for(int j = 0; j < mat.length; j++){
 					if(j != 0){
 						System.out.println(mat[i][j]);
+						gravarArquivo.println(mat[i][j]);
 					}
 				}
 			}
+			System.out.println("Arquivo gravado com sucesso!");
+			arquivo.close();
 			entrada.close();
 	}
 
