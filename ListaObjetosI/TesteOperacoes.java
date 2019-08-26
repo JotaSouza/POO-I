f6e0acea189b227df0f1fbe21344a97c99c6f4a2
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-08-25 22:20:17 -0300

    Create TesteOperacoes.java

---
+++ ListaObjetosI/TesteOperacoes.java
@@ -0,0 +1,15 @@
+package ListaObjetosI;
+
+public class TesteOperacoes {
+
+	public static void main(String[] args) {
+		
+		Operacoes op = new Operacoes();
+		
+		op.setNum1(10);
+		op.setNum2(2);
+		op.operacoesNumeros();
+
+	}
+
+}
