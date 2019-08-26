7b4d4bf00b44a7491d17c9332d74213b6913946c
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-08-25 22:19:49 -0300

    Create NomeCompleto.java

---
+++ ListaObjetosI/NomeCompleto.java
@@ -0,0 +1,40 @@
+package ListaObjetosI;
+
+public class NomeCompleto {
+
+	private String nome;
+	private String sobrenome;
+	
+	public NomeCompleto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void nome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void sobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+
+	
+	
+	
+}
