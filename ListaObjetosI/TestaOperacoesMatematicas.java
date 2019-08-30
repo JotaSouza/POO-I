18727bc97c18e318fc2cabc890b84e4f3317d0e7
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-08-30 15:57:44 -0300

    Create TestaOperacoesMatematicas.java

---
+++ ListaObjetosI/TestaOperacoesMatematicas.java
@@ -0,0 +1,16 @@
+package ListaObjetos;
+
+public class TestaOperacoesMatematicas {
+
+	public static void main(String[] args) {
+		
+		OperacoesMatematicas n = new OperacoesMatematicas();
+		
+		n.setNum1(10);
+		n.setNum2(2);
+		
+		n.operacoesMatematicas();
+
+	}
+
+}
