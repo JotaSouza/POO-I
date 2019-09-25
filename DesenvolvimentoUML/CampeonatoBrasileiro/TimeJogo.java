0c5bf9dcc4ee344289d4b2bd616c8c244547fc29
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-25 16:20:09 -0300

    Create TimeJogo.java

---
+++ DesenvolvimentoUML/CampeonatoBrasileiro/TimeJogo.java
@@ -0,0 +1,27 @@
+package DesenvolvimentoUML.CampeonatoBrasileiro;
+
+public class TimeJogo extends Time {
+
+	private int gol;
+
+	public int getGol() {
+		return gol;
+	}
+
+	public void setGol(int gol) {
+		this.gol = gol;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TimeJogo [gol=");
+		builder.append(gol);
+		builder.append(", toString()=");
+		builder.append(super.toString());
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
