35a7037ad6a652eb02787cdf0717bd0b7c7a64f0
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-25 16:20:41 -0300

    Create Pessoa.java

---
+++ DesenvolvimentoUML/SisAcademico/Pessoa.java
@@ -0,0 +1,33 @@
+package DesenvolvimentoUML.SisAcademico;
+
+public class Pessoa {
+
+	private String nome;
+	private int cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if(nome != null)
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pessoa [nome=");
+		builder.append(nome);
+		builder.append(", cpf=");
+		builder.append(cpf);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
