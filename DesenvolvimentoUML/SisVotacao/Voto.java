37b3c776e8cc2699b3a28b59f5e777ac9047f4be
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-09-25 16:21:01 -0300

    Create Voto.java

---
+++ DesenvolvimentoUML/SisVotacao/Voto.java
@@ -0,0 +1,25 @@
+package DesenvolvimentoUML.SisVotacao;
+
+public class Voto {
+
+	protected Votacao votacao;
+
+	public Votacao getVotacao() {
+		return votacao;
+	}
+
+	public void setVotacao(Votacao votacao) {
+		this.votacao = votacao;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Voto [votacao=");
+		builder.append(votacao);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
