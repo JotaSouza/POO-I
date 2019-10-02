53fafc288ed8c157b3a648969897f7988724968b
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-10-02 16:37:29 -0300

    Update MainListaDias.java

--- Datas/MainListaDias.java
+++ Datas/MainListaDias.java
@@ -2,17 +2,21 @@ package Datas;
 
 import java.util.Calendar;
 import java.util.Date;
-//import java.util.Iterator;
-//import java.util.List;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainListaDias {
 
 	public static void main(String[] args) {
 		
 		
-		//List<Date> lista = ListaDatas.retornaData(8, 2019);
-	//	for(Iterator iter = lista.iterator());
+		List<Date> lista = ListaDatas.retornaData(9, 2019);
+		for(Iterator<Date> iter = lista.iterator(); iter.hasNext();){
+			Date elemento = (Date) iter.next();
+			System.out.println(elemento);
+		}
 
+		System.out.println("");
 		Calendar calendario = Calendar.getInstance();
 		calendario.set(Calendar.DAY_OF_MONTH, 28);
 		calendario.set(Calendar.MONTH, 7);
@@ -26,6 +30,23 @@ public class MainListaDias {
 		
 		long segundos = ListaDatas.segundosEntreDatas(date, hoje);
 		System.out.println(segundos);
+		
+		System.out.println("");
+		
+		Calendar calendario2 = Calendar.getInstance();
+		calendario2.set(Calendar.DAY_OF_MONTH, 28);
+		calendario2.set(Calendar.MONTH, 7);
+		calendario2.set(Calendar.YEAR, 1984);
+		calendario2.set(Calendar.HOUR_OF_DAY, 12);
+		calendario2.set(Calendar.MINUTE, 03);
+		calendario2.set(Calendar.SECOND, 27);
+		Date data = calendario.getTime();
+		
+		Date hoj = new Date();
+		
+		double dias = ListaDatas.diferencaEmDias(data, hoj);
+		System.out.println(dias);
+		
 	}
 
 }
