111016a756afcf4769c112676483c16342b3d08a
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-10-10 11:37:34 -0300

    Create IControle.java

---
+++ Interfaces/IControle.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public interface IControle {
+
+	public boolean ligaDesliga();
+	public int alterarCanal(int canal);
+	public int canalMais();
+	public int canalMenos();
+	public int volumeMais();
+	public int volumeMenos();
+}
