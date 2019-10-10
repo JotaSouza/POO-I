ad9895e75e3cf3da9bf0773f15ae928464f6b1e7
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-10-10 11:37:48 -0300

    Create MainCachorro.java

---
+++ Polimorfismo/MainCachorro.java
@@ -0,0 +1,11 @@
+package Polimorfismo;
+
+public class MainCachorro {
+
+	public static void main(String[] args) {
+		Animal animal = new Cachorro();
+		System.out.println(animal.falar());
+
+	}
+
+}
