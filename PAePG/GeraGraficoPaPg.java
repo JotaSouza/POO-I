286577e94561e2bbde247746c11b896cc29ca168
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-11-06 18:36:47 -0300

    Create GeraGraficoPaPg.java

---
+++ PAePG/GeraGraficoPaPg.java
@@ -0,0 +1,15 @@
+package PAePG;
+
+public class GeraGraficoPaPg {
+
+	private double vetor[];
+
+	public double[] getVetor() {
+		return vetor;
+	}
+
+	public void setVetor(double[] vetor) {
+		this.vetor = vetor;
+	}
+	
+}
